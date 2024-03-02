06d97cdfaa60c28ec57d0ad83620b00d0d00e669
Annika Heuler-Kottmann <annikaheuler@gmx.de>
2024-03-02 12:06:31 +0100

    add crawler to extract links

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module web-crawler
 
 go 1.22.0
+
+require golang.org/x/net v0.21.0
--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			panic(err)
+		}
+
+		defer resp.Body.Close()
+		links := extractLinks(resp.Body)
+		for _, link := range links {
+			fmt.Println(link)
+		}
+	}
+}
+
+func extractLinks(r io.Reader) []string {
+	links := make([]string, 0)
+	tokenizer := html.NewTokenizer(r)
+
+	for {
+		tokenType := tokenizer.Next()
+
+		switch tokenType {
+		case html.ErrorToken:
+			return links
+		case html.StartTagToken, html.SelfClosingTagToken:
+			token := tokenizer.Token()
+
+			if token.Data == "a" {
+				for _, attr := range token.Attr {
+					if attr.Key == "href" {
+						if !strings.HasPrefix(attr.Val, "#") {
+							links = append(links, attr.Val)
+						}
+					}
+				}
+			}
+		}
+	}
 }
